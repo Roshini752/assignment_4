ae8bdb79ee02131535ec1e748ae053d24548b3a8
Roshini752 <88433698+Roshini752@users.noreply.github.com>
2021-08-16 17:08:02 +0530

    concatenate without using functions

--- concatenate.c
+++ concatenate.c
@@ -2,11 +2,20 @@
 #include<string.h>
 int main()
 {
-    char str1[]="roshini";
-    char str2[]="peyyala";
-    strcat(str1,str2);
-    printf("merged=%s\n",str1);
-    int l=strlen(str1);
-    printf("length of concatemated string is %d\n",l);
-
+    char str1[]="thunder";
+    char str2[]="soft";
+    int i=0;
+    while(str1[i] != '\0')
+    {
+        i++;
+    }
+    int j=0;
+    while(str2[j] != '\0')
+    {
+        str1[i]=str2[j];
+        j++;
+        i++;
+    }
+    str1[i]='\0';
+    printf("%s",str1);
 }
